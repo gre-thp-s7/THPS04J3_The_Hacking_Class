196c8488bc719688982a7c9b2e9b51c91e554336
carlaight <charlesmix23@gmail.com>
2019-01-30 17:56:06 +0100

    update :

--- db/migrate/20190130155507_create_students.rb
+++ db/migrate/20190130155507_create_students.rb
@@ -2,7 +2,7 @@ class CreateStudents < ActiveRecord::Migration[5.2]
   def change
     create_table :students do |t|
       t.text :name
-      t.belongs_to :courses, index true
+      t.belongs_to :courses, index: true
       t.timestamps
     end
   end
