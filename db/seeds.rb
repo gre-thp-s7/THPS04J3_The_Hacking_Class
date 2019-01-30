87dc90992dd4fbc460cf4a8ea5532fe1d691d5d6
Maxime Audfray <maxime.audfray@gmail.com>
2019-01-30 20:29:39 +0100

    add mother fokingue seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# optional: true
+
+require 'faker'
+
+Course.destroy_all
+Student.destroy_all
+
+10.times do
+  course = Course.create!(name: Faker::StarWars.unique.wookiee_sentence)
+end
+
+30.times do
+  student = Student.create!(name: Faker::StarWars.unique.character, courses_id: rand(1..10))
+end
